03fde152dff86dfa25ad318cd2ed7c4aaf034411
Akshay_Holkar <aholkar01@gmail.com>
2024-06-05 14:34:31 +0530

    maven practice commit

---
+++ mavenpractice/src/test/java/mavenpractice/suiteTest.java
@@ -0,0 +1,11 @@
+package mavenpractice;
+
+import org.testng.annotations.Test;
+
+public class suiteTest {
+  @Test
+  public void Demo() {
+	  System.out.println("class for batch execution");
+	  
+  }
+}
